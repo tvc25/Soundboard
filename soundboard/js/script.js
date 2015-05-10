64d60d4c80dce312ae456e73b3b6ea29ebe79103
Tobia Cassandro <tobiacassandro@gmail.com>
2015-05-10 16:56:13 +0100

    added hover effects to the numbers

---
+++ soundboard/js/script.js
@@ -0,0 +1,162 @@
+//ONE//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#nine").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+// TWO //
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#one").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+// TWO //
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#two").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+//THREE//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#three").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+//FOUR//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#four").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+//FIVE//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#one").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+//SIX//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#one").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+//SEVEN//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#one").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+
+//EIGHT//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#one").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
+
+//NINE//
+soundManager.setup({
+ url: '/path/to/swf-files/',
+ onready: function() {
+
+   $("#one").on("click", function(){
+ var mySound = soundManager.createSound({
+ url: 'audiofiles/ok.mp3'
+ });
+ mySound.play();
+
+})
+ },
+ ontimeout: function() {
+ }
+});
